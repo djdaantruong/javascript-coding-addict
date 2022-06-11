e37cb1d1866f7a1b80614592188b938cbf6e8984
Djdaan Truong <daantd.a12017@gmail.com>
2022-06-11 14:06:29 +0700

    color flipper done

--- dom-project/01-color-flipper/setup/hex.js
+++ dom-project/01-color-flipper/setup/hex.js
@@ -1,1 +1,17 @@
-const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F']
+
+const btn = document.getElementById('btn')
+const color = document.querySelector('.color')
+
+btn.addEventListener('click', function () {
+  let newColor = '#'
+  for (let i = 0; i < 6; i++) {
+    newColor += hex[getRandomColor()]
+  }
+  document.body.style.backgroundColor = newColor
+  color.textContent = newColor
+})
+
+function getRandomColor() {
+  return Math.floor(Math.random() * hex.length)
+}
