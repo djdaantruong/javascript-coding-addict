6e2b75ed71ce1aaa8dbb702984e2e60380a9c8b1
Djdaan Truong <daantd.a12017@gmail.com>
2022-08-01 16:55:28 +0700

    scroll section complete

--- dom-project/10-scroll/setup/app.js
+++ dom-project/10-scroll/setup/app.js
@@ -24,7 +24,56 @@ navToggle.addEventListener('click', () => {
   }
 })
 
+const navbar = document.getElementById('nav')
+const topLink = document.querySelector('.top-link')
 // ********** fixed navbar ************
+window.addEventListener('scroll', () => {
+  const scrollHeight = window.pageYOffset
+  console.log(scrollHeight)
+  const navHeight = navbar.getBoundingClientRect().height
+  console.log(navHeight)
+  if (scrollHeight > navHeight) {
+    navbar.classList.add('fixed-nav')
+  } else {
+    navbar.classList.remove('fixed-nav')
+  }
+
+  if (scrollHeight > 500) {
+    topLink.classList.add('show-link')
+  } else {
+    topLink.classList.remove('show-link')
+  }
+})
 
 // ********** smooth scroll ************
 // select links
+const scrollLinks = document.querySelectorAll('.scroll-link')
+
+scrollLinks.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    // prevent default
+    e.preventDefault()
+    // navigate to specific spot
+    const id = e.currentTarget.getAttribute('href').slice(1)
+    const element = document.getElementById(id)
+
+    // calculate the heights
+    const navHeight = navbar.getBoundingClientRect().height
+    const containerHeight = linksContainer.getBoundingClientRect().height
+    const fixedNav = navbar.classList.contains('fixed-nav')
+
+    let position = element.offsetTop - navHeight
+    if (!fixedNav) {
+      position = position - navHeight
+    }
+    if (navHeight > 82) {
+      position = position + containerHeight
+    }
+
+    window.scrollTo({
+      left: 0,
+      top: position,
+    })
+    linksContainer.style.height = 0
+  })
+})
